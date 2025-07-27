4f0057e8c09900f13436a62a042e9eb7edb26e97
Surendra Raut <surendra.raut@gmail.com>
2025-07-27 18:01:19 -0400

    Initial commit.

---
+++ src/main/java/me/suren/db_api/DbApiApplication.java
@@ -0,0 +1,13 @@
+package me.suren.db_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbApiApplication.class, args);
+	}
+
+}
